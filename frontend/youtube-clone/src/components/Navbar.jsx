61b88fb77093f3c60beff5db26003038b987ab8a
sana2990 <sanaasad2990@gmail.com>
2026-06-08 07:54:07 +0530

    Added empty components pages

---
+++ frontend/youtube-clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>YouTube Clone</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/youtube-clone/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+function Sidebar() {
+  return (
+    <aside>
+      <p>Home</p>
+      <p>Subscriptions</p>
+      <p>Library</p>
+    </aside>
+  );
+}
+
+export default Sidebar;
