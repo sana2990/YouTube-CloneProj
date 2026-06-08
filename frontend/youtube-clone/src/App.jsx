f04e70fe06f3ab62d50d6a022a17e9044c9c9226
sana2990 <sanaasad2990@gmail.com>
2026-06-08 07:50:46 +0530

    Added routes in app file.

--- frontend/youtube-clone/src/App.jsx
+++ frontend/youtube-clone/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/register" element={<RegisterPage />} />
+  
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
