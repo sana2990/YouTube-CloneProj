f2087d61d382175a425b4a2494f91c97e39f33f4
sana2990 <sanaasad2990@gmail.com>
2026-06-08 08:25:24 +0530

    Added navbar code

--- frontend/youtube-clone/src/components/Navbar.jsx
+++ frontend/youtube-clone/src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 function Navbar() {
   return (
-    <nav>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "15px",
+        borderBottom: "1px solid #ddd",
+      }}
+    >
       <h2>YouTube Clone</h2>
+
+      <input
+        type="text"
+        placeholder="Search videos..."
+        style={{
+          width: "400px",
+          padding: "8px",
+        }}
+      />
+
+      <button>
+        Sign In
+      </button>
     </nav>
   );
 }
